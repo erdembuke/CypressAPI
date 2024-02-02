7bcb726c42a8e4f6de7e771b8feeab7597ea57e8
erdembuke <erdembke@gmail.com>
2024-02-02 22:31:50 +0300

    first complete test is added with cy.intercept() method and assertions also

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -1,11 +1,31 @@
+/// <reference types="cypress" />
+
 describe('Test with backend', () => {
 
   beforeEach('login to application', () => {
     cy.loginToApplication()
   })
 
-  it('first', () => {
-    cy.log('Yaaaay we logged in!')
+  it.only('verify correct request and response', () => {
+
+    // 1. define the intercept before the call
+    cy.intercept('POST', 'https://conduit-api.bondaracademy.com/api/articles/').as('postArticles') // we need to use intercept method before the steps.
+
+    // 2. perform the api call
+    cy.contains('New Article').click()
+    cy.get('[formcontrolname="title"]').type('This is the title')
+    cy.get('[formcontrolname="description"]').type('This is a description')
+    cy.get('[formcontrolname="body"]').type('This is a body of the article')
+    cy.contains('Publish Article').click()
+
+    // 3. validation
+    cy.wait('@postArticles').then( xhr => {
+      console.log(xhr)
+      expect(xhr.response.statusCode).to.equal(201)
+      expect(xhr.request.body.article.body).to.equal('This is a body of the article')
+      expect(xhr.response.body.article.description).to.equal('This is a description')
+    })
+
   })
 
 

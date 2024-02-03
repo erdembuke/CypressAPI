19fd18e117837449d2c756f2d326d0b39c55807b
erdembuke <erdembke@gmail.com>
2024-02-03 21:04:08 +0300

    api calls with cypress, providing cy.request() with parameters, writing assertions

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -3,7 +3,7 @@
 describe('Test with backend', () => {
 
   beforeEach('login to application', () => {
-    cy.intercept({method: 'GET', path: 'tags'}, {fixture: 'tags.json'})
+    cy.intercept({ method: 'GET', path: 'tags' }, { fixture: 'tags.json' })
     cy.loginToApplication()
   })
 
@@ -20,7 +20,7 @@ describe('Test with backend', () => {
     cy.contains('Publish Article').click()
 
     // 3. validation
-    cy.wait('@postArticles').then( xhr => {
+    cy.wait('@postArticles').then(xhr => {
       console.log(xhr)
       expect(xhr.response.statusCode).to.equal(201)
       expect(xhr.request.body.article.body).to.equal('This is a body of the article')
@@ -28,7 +28,7 @@ describe('Test with backend', () => {
     })
   })
 
-  it.only('intercepting and modifying the request and response', () => {
+  it('intercepting and modifying the request and response', () => {
 
     // 1. define the intercept before the call
     cy.intercept('POST', 'https://conduit-api.bondaracademy.com/api/articles/', (req) => {
@@ -43,7 +43,7 @@ describe('Test with backend', () => {
     cy.contains('Publish Article').click()
 
     // 3. validation
-    cy.wait('@postArticles').then( xhr => {
+    cy.wait('@postArticles').then(xhr => {
       console.log(xhr)
       expect(xhr.response.statusCode).to.equal(201)
       expect(xhr.request.body.article.body).to.equal('This is a body of the article')
@@ -56,15 +56,15 @@ describe('Test with backend', () => {
     // we create new file in fixtures folder as mock datas, added cy.intercept() into beforeEach method.
     cy.log('we logged in')
     cy.get('.tag-list')
-    .should('contain', 'cypress')
-    .and('contain', 'automation')
-    .and('contain', 'testing')
+      .should('contain', 'cypress')
+      .and('contain', 'automation')
+      .and('contain', 'testing')
 
   })
 
   it('verify global feed likes count', () => {
-    cy.intercept('GET', 'https://conduit-api.bondaracademy.com/api/articles/feed*', {"articles":[],"articlesCount":0})
-    cy.intercept('GET', 'https://conduit-api.bondaracademy.com/api/articles*', {fixture: 'articles.json'})
+    cy.intercept('GET', 'https://conduit-api.bondaracademy.com/api/articles/feed*', { "articles": [], "articlesCount": 0 })
+    cy.intercept('GET', 'https://conduit-api.bondaracademy.com/api/articles*', { fixture: 'articles.json' })
 
     cy.contains('Global Feed').click()
     cy.get('app-article-list button').then(heartList => {
@@ -84,7 +84,55 @@ describe('Test with backend', () => {
 
   })
 
-  
+  it('delete a new article in a global feed', () => {
+
+    const userCredentials = {
+      "user": {
+        "email": "erdem@test.com",
+        "password": "erdembke"
+      }
+    }
+
+    const bodyRequest = {
+      "article": {
+        "title":"Cypress Article Title",
+        "description":"Cypress Article Description",
+        "body":"Cypress Article Body",
+        "tagList":[]
+      }
+    }
+
+    cy.request('POST', 'https://conduit-api.bondaracademy.com/api/users/login', userCredentials)
+    .its('body').then(body => {
+      const token = body.user.token
+
+      cy.request({
+        url: 'https://conduit-api.bondaracademy.com/api/articles/',
+        headers: { 'Authorization': 'Token ' + token},
+        method: 'POST',
+        body: bodyRequest
+      }).then( response => {
+        expect(response.status).to.equal(201)
+      })
+
+    cy.contains('Global Feed').click()
+    cy.wait(500)
+    cy.get('.article-preview').first().click()
+    cy.get('.article-actions').contains('Delete Article').click()
+
+    cy.request({
+      url: 'https://conduit-api.bondaracademy.com/api/articles?limit=10&offset=0',
+      headers: { 'Authorization': 'Token ' + token},
+      method: 'GET'
+    }).its('body').then(body => {
+      expect(body.articles[0].title).not.to.equal('Cypress Article Title')
+    })
+    })
+
+
+  })
+
+
 
 
 })
